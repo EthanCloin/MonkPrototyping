16ff7a5116e7735403c71c86cc2e2b55e9b2867b
Ethan Cloin <psnethan@gmail.com>
2021-11-04 17:22:52 -0400

    made a wisp sprite and working on collection script

---
+++ Assets/Scripts/CollectableBehavior.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollectableBehavior : MonoBehaviour
+{
+    public int wispsCollected;
+    // Start is called before the first frame update
+    void Start()
+    {
+        wispsCollected = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Destroy(gameObject);
+        
+        wispsCollected++;
+    }
+}
