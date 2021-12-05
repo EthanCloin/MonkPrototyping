4048e6aef1a3ac36bbff5d03d27a3d0e9823e21c
Ethan Cloin <psnethan@gmail.com>
2021-12-05 17:51:25 -0500

    Implemented RefactoredHealth

---
+++ Assets/Scripts/Player_Related/RefactoredHealth.cs
@@ -0,0 +1,56 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// This class manages health information and passes it along
+/// to the Manager to update display
+///
+/// Singleton
+/// 
+/// </summary>
+public class RefactoredHealth
+{
+    private static RefactoredHealth playerHealth = null;
+    private int currentHealth;
+    public int maximumHealth = 3;
+   
+    // constructor
+    private RefactoredHealth()
+    {
+        currentHealth = maximumHealth;
+    }
+
+    // instance method ensures only 1 health object exists
+    public static RefactoredHealth getInstance()
+    {
+        if (playerHealth == null)
+            playerHealth = new RefactoredHealth();
+
+        return playerHealth;
+    }
+
+    /// <summary>
+    /// Call this function to decrement and return the health value
+    /// </summary>
+    /// <returns>int currentHealth after taking damage</returns>
+    public int TakeOneDamage()
+    {
+        return currentHealth--;
+    }
+
+    /// <summary>
+    /// Call this function to check current health
+    /// </summary>
+    /// <returns>int currentHealth</returns>
+    public int GetCurrentHealth()
+    {
+        return currentHealth;
+    }
+
+    public void RestoreFullHealth()
+    {
+        currentHealth = maximumHealth;
+    }
+
+}
